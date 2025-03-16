56640d5ba9593167e0ad019595b25ebd5862e4f4
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-03-16 19:38:01 +0300

    initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import time
+
+from lark import Lark
+
+# with open("grammar.lark") as f1, open("grammar_test.lua") as f2:
+with open("grammar.lark") as f1, open("test.lua") as f2:
+    s_grammar = f1.read()
+    s_input = f2.read()
+
+
+lark = Lark(s_grammar, parser="earley")
+start = time.time()
+tree = lark.parse(s_input)
+end = time.time()
+print(tree.pretty())
+print(f'Done in {round(end - start, 3)} seconds.')
