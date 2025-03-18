b7c6eb3302ccb12d3198bff6547ddf084cf0c44a
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-03-18 03:04:06 +0300

    WIP: very basic bytecode

--- luark_ast.py
+++ luark_ast.py
@@ -1,35 +1,51 @@
-import math
 from dataclasses import dataclass
 from typing import List
 
 from lark import ast_utils, Transformer
 
-environment = {}
+
+class Prototype:
+    def __init__(self):
+        self.consts: List[str] = []
+        self.opcodes: List[str] = []
+
+    def __str__(self):
+        result = ["constants:"]
+        for i, c in enumerate(self.consts):
+            name = str(c)
+            if isinstance(c, str):
+                name = '"' + name + '"'
+            result.append(f"\t{i}\t\t{name}")
+        result.append("instructions:")
+        for i, o in enumerate(self.opcodes):
+            result.append(f"\t{i}\t\t{o}")
+        return "\n".join(result)
+
+    def get_const(self, const):
+        if const in self.consts:
+            return self.consts.index(const)
+        else:
+            index = len(self.consts)
+            self.consts.append(const)
+            return index
+
+    def add_opcode(self, opcode):
+        self.opcodes.append(opcode)
 
 
 class _Ast(ast_utils.Ast):
-    def evaluate(self):
+    def evaluate(self, *args, **kwargs):
         raise NotImplementedError
 
 
-class Statement(_Ast):
-    pass
+@dataclass
+class Var(_Ast):
+    name: str
 
-
-class NameList(_Ast):
-    pass
-
-
-class Field(_Ast):
-    pass
-
-
-class FieldList(_Ast):
-    pass
-
-
-class TableConstructor(_Ast):
-    pass
+    def evaluate(self, *args, **kwargs):
+        proto = args[0]
+        idx = proto.get_const(self.name)
+        proto.add_opcode(f"load {idx}")
 
 
 class Expr(_Ast):
@@ -37,321 +53,101 @@ class Expr(_Ast):
 
 
 @dataclass
-class Var(Expr):
-    name: str
+class String(Expr):
+    value: str
 
-    def evaluate(self):
-        return environment.get(self.name, None)
+    def evaluate(self, *args, **kwargs):
+        proto: Prototype = args[0]
+        idx = proto.get_const(self.value)
+        proto.add_opcode(f"pushc {idx}")
 
 
 @dataclass
 class Number(Expr):
     value: float
 
-    def evaluate(self):
-        return self.value
-
-
-@dataclass
-class String(Expr):
-    value: str
-
-    def evaluate(self):
-        return self.value
-
-
-@dataclass
-class BinaryOp:
-    left: Expr
-    right: Expr
-
-
-class OrExpr(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() or self.right.evaluate()
-
-
-class AndExpr(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() and self.right.evaluate()
-
-
-class CompLt(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() < self.right.evaluate()
-
-
-class CompGt(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() > self.right.evaluate()
-
-
-class CompLe(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() <= self.right.evaluate()
-
-
-class CompGe(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() >= self.right.evaluate()
-
-
-class CompEq(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() == self.right.evaluate()
-
-
-class CompNeq(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() != self.right.evaluate()
-
-
-class BwOrExpr(Expr):
-    pass
-
-
-class BwXorExpr(Expr):
-    pass
-
-
-class BwAndExpr(Expr):
-    pass
-
-
-class LshExpr(Expr):
-    pass
-
-
-class RshExpr(Expr):
-    pass
-
-
-class AddExpr(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() + self.right.evaluate()
-
-
-class SubExpr(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() - self.right.evaluate()
-
-
-class MulExpr(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() * self.right.evaluate()
-
-
-class DivExpr(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() / self.right.evaluate()
-
-
-class FdivExpr(Expr, BinaryOp):
-    def evaluate(self):
-        return math.floor(self.left.evaluate() / self.right.evaluate())
-
-
-class ModExpr(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() % self.right.evaluate()
-
-
-@dataclass
-class UnaryOp:
-    operand: Expr
-
-
-class UnaryMinus(Expr, UnaryOp):
-    def evaluate(self):
-        return -self.operand.evaluate()
-
-
-class UnaryNot(Expr, UnaryOp):
-    def evaluate(self):
-        return not self.operand.evaluate()
-
-
-class UnaryLength(Expr, UnaryOp):
-    def evaluate(self):
-        raise NotImplementedError
-
-
-class UnaryBwNot(Expr, UnaryOp):
-    def evaluate(self):
-        return ~self.operand.evaluate()
-
-
-class ExpExpr(Expr, BinaryOp):
-    def evaluate(self):
-        return self.left.evaluate() ** self.right.evaluate()
+    def evaluate(self, *args, **kwargs):
+        proto: Prototype = args[0]
+        idx = proto.get_const(self.value)
+        proto.add_opcode(f"pushc {idx}")
 
 
 @dataclass
 class ExprList(_Ast, ast_utils.AsList):
     exprs: List[Expr]
 
-    def evaluate(self):
-        result = []
+    def evaluate(self, *args, **kwargs):
         for e in self.exprs:
-            if isinstance(e, Expr):
-                result.append(e.evaluate())
-            else:
-                result.append(e)
-        return result
-
-
-class EmptyStmt(Statement):
-    pass
+            e.evaluate(*args, **kwargs)
 
 
 @dataclass
-class AssignStmt(Statement):
-    var_list: List[str]
-    expr_list: ExprList
+class AddExpr(Expr):
+    left: Expr
+    right: Expr
 
-    def evaluate(self):
-        values = self.expr_list.evaluate()
-        for n, v in zip(self.var_list, values):
-            environment[n] = v
-
-
-class LocalStmt(Statement):
-    pass
-
-
-class FuncDefStmt(Statement):
-    pass
+    def evaluate(self, *args, **kwargs):
+        proto = args[0]
+        if (isinstance(self.left, Number)
+                and isinstance(self.right, Number)):
+            result = self.left.value + self.right.value
+            idx = proto.get_const(result)
+            proto.add_opcode(f"pushc {idx}")
+        else:
+            self.left.evaluate(*args, **kwargs)
+            self.right.evaluate(*args, **kwargs)
+            proto.add_opcode("add")
 
 
 @dataclass
 class FuncCall(_Ast):
-    primary: Expr
-    params: ExprList | TableConstructor | str
+    primary: str
+    params: ExprList = None
 
-    def evaluate(self):
-        if isinstance(self.primary, Var) and self.primary.name == "print":
-            if isinstance(self.params, ExprList):
-                values = self.params.evaluate()
-                for i in range(len(values)):
-                    if values[i] is None:
-                        values[i] = 'nil'
-                print(*values)
+    def evaluate(self, *args, **kwargs):
+        proto: Prototype = args[0]
+        my_idx = proto.get_const(self.primary)
 
+        # exprs = self.params.evaluate(*args, **kwargs)
+        # for e in exprs:
+        #     idx = proto.get_const(e)
+        #     proto.add_opcode(f"load {idx}")
 
-class WhileStmt(Statement):
-    pass
+        # for p in self.params:
+        #     p.evaluate(*args, **kwargs)
 
+        if self.params:
+            self.params.evaluate(*args, **kwargs)
 
-class RepeatStmt(Statement):
-    pass
-
-
-class BreakStmt(Statement):
-    pass
+        proto.add_opcode(f"call {my_idx}")
 
 
-class ForLoopNum(Statement):
-    pass
+@dataclass
+class AssignStmt(_Ast):
+    var_list: List[str]
+    expr_list: ExprList
 
-
-class ForLoopGen(Statement):
-    pass
-
-
-class Label(Statement):
-    pass
-
-
-class GotoStmt(Statement):
-    pass
-
-
-class ReturnStmt(Statement):
-    pass
+    def evaluate(self, *args, **kwargs):
+        proto: Prototype = args[0]
+        self.expr_list.evaluate(*args, **kwargs)
+        for v in self.var_list[::-1]:
+            idx = proto.get_const(v)
+            proto.add_opcode(f"store {idx}")
 
 
 @dataclass
 class Chunk(_Ast, ast_utils.AsList):
-    statements: List[Statement | ReturnStmt]
+    stmts: List[FuncCall]
 
-    def evaluate(self):
-        for stmt in self.statements:
-            stmt.evaluate()
-
-
-@dataclass
-class Elseif(_Ast):
-    expr: Expr
-    body: Chunk
-
-
-class IfStmt(Statement, ast_utils.AsList):
-    def __init__(self, children):
-        self.expr = children[0]
-        self.body = children[1]
-        self.elseifs = []
-        self.else_body = None
-        for c in children[2:]:
-            if isinstance(c, Elseif):
-                self.elseifs.append(c)
-            elif isinstance(c, Chunk):
-                if self.else_body:
-                    raise RuntimeError
-                self.else_body = c
-
-    def evaluate(self):
-        value = self.expr.evaluate()
-        if value:
-            self.body.evaluate()
-        else:
-            if self.elseifs:
-                for e in self.elseifs:
-                    value = e.expr.evaluate()
-                    if value:
-                        e.body.evaluate()
-                        return
-
-            if self.else_body:
-                self.else_body.evaluate()
-
-
-class Attribute(_Ast):
-    pass
-
-
-class AttrNameList(Statement):
-    pass
-
-
-class FuncDef(Expr):
-    pass
-
-
-class FuncBody(_Ast):
-    pass
-
-
-class ParamList(_Ast):
-    pass
-
-
-class FuncName(_Ast):
-    pass
+    def evaluate(self, *args, **kwargs):
+        proto = Prototype()
+        for s in self.stmts:
+            s.evaluate(proto)
+        proto.add_opcode("return")
+        print(proto)
 
 
 class ToAst(Transformer):
-    def var_list(self, c):
-        return [c.name for c in c]
-
-    def true(self, _):
-        return True
-
-    def false(self, _):
-        return False
-
-    def Nil(self, _):
-        return None
-
     def DEC_NUMBER(self, n):
         return float(n)
 
@@ -370,3 +166,6 @@ class ToAst(Transformer):
 
     def ID(self, s):
         return str(s)
+
+    def var_list(self, l):
+        return [v.name for v in l]
---
+++ luavm.py
@@ -0,0 +1,4 @@
+class LuaVM:
+    def __init__(self):
+        self.call_stack = []
+        self.env = {}
