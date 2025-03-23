738a3a8e8b6171672c365dbc2bf0bb8674f7c7a2
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-03-23 23:50:04 +0300

    add literals

--- luark/compiler/luark_ast.py
+++ luark/compiler/luark_ast.py
@@ -1,7 +1,8 @@
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 from lark.ast_utils import Ast, AsList
-from lark.visitors import Transformer
+from lark.visitors import Transformer, Discard
 
 from luark.compiler.program import Program, Prototype
 
@@ -12,8 +13,46 @@ class _State:
         self.proto = program.main_proto
 
 
-class Statement:
+class Statement(ABC):
+    @abstractmethod
     def emit(self, state: _State):
+        pass
+
+
+class Expression(ABC):
+    @abstractmethod
+    def evaluate(self, state: _State):
+        pass
+
+
+@dataclass
+class String(Ast, Expression):
+    value: str
+
+    def evaluate(self, state: _State):
+        pass
+
+
+@dataclass
+class Number(Ast, Expression):
+    value: int | float
+
+    def evaluate(self, state: _State):
+        pass
+
+
+class NilValue(Expression):
+    def evaluate(self, state: _State):
+        pass
+
+
+class TrueValue(Expression):
+    def evaluate(self, state: _State):
+        pass
+
+
+class FalseValue(Expression):
+    def evaluate(self, state: _State):
         pass
 
 
@@ -42,3 +81,62 @@ class Chunk(Ast):
 class LuarkTransformer(Transformer):
     def start(self, children):
         return children[-1]
+
+    def dec_int(self, n):
+        num: str = n[0]
+        num: list[str] = num.casefold().split("e")
+        if len(num) == 1:
+            return Number(int(num[0]))
+        elif len(num) == 2:
+            return Number(int(num[0]) * 10 ** int(num[1]))
+        else:
+            raise Exception(f"Illegal decimal integer literal: '{n}'")
+
+    def dec_float(self, f):
+        num: str = f[0]
+        num: list[str] = num.casefold().split("e")
+        if len(num) == 1:
+            return Number(float(num[0]))
+        elif len(num) == 2:
+            return Number(float(num[0]) * 10 ** float(num[1]))
+        else:
+            raise Exception(f"Illegal decimal float literal: '{f}'")
+
+    def empty_stmt(self, _):
+        return Discard
+
+    def nil(self, _):
+        return NilValue()
+
+    def true(self, _):
+        return TrueValue()
+
+    def false(self, _):
+        return FalseValue()
+
+    def ID(self, s):
+        return str(s)
+
+    # TODO: raise error on unknown escape sequences
+    # TODO: support \xXX, \ddd, \u{XXX}
+    def STRING(self, s):
+        s = ''.join([s.strip() for s in s.split("\\z")])
+        s = (str(s)[1:-1]
+             .replace("\\a", "\a")
+             .replace("\\b", "\b")
+             .replace("\\f", "\f")
+             .replace("\\n", "\n")
+             .replace("\\r", "\r")
+             .replace("\\t", "\t")
+             .replace("\\v", "\v")
+             .replace("\\\\", "\\")
+             .replace("\\\"", "\"")
+             .replace("\\\'", "\'")
+             .replace("\\\n", "\n"))
+        return s
+
+    def MULTISTRING(self, s):
+        raise NotImplementedError
+        # s = str(s)
+        # size = s.find("[", 1) + 1
+        # return s[size:-size]
