dcac7476de023f5c8ea88f8b8e15d524d9ee12c9
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-04-04 14:48:47 +0300

    refactor local opcodes

--- luark/opcode/local.py
+++ luark/opcode/local.py
@@ -2,52 +2,41 @@ from luark.opcode import Opcode
 from luark.program import Program, Prototype
 
 
-def _format_local(index: int, proto: Prototype) -> str:
-    local = proto.locals.by_index(index)
-    name = local.name if local.name is not None else "(temp)"
-    return f"{name}[{index}]"
-
-
-class LoadLocal(Opcode):
+class LocalOpcode(Opcode):
     index: int
 
-    def __init__(self, index: int):
-        super().__init__("load_local")
+    def __init__(self, name: str, index: int):
+        super().__init__(name)
         self.index = index
+
+    def comment_str(self, program: Program, proto: Prototype, pc: int) -> str:
+        local = proto.locals.by_index(self.index)
+        name = local.name if local.name is not None else "(temp)"
+        return f"{name}[{self.index}]"
+
+
+class LoadLocal(LocalOpcode):
+    def __init__(self, index: int):
+        super().__init__("load_local", index)
 
     @property
     def arg_str(self) -> str:
         return f"{self.index}"
 
-    def comment_str(self, program: Program, proto: Prototype, pc) -> str:
-        return _format_local(self.index, proto)
 
-
-class StoreLocal(Opcode):
-    index: int
-
+class StoreLocal(LocalOpcode):
     def __init__(self, index: int):
-        super().__init__("store_local")
-        self.index = index
+        super().__init__("store_local", index)
 
     @property
     def arg_str(self) -> str:
         return f"{self.index}"
 
-    def comment_str(self, program: Program, proto: Prototype, pc) -> str:
-        return _format_local(self.index, proto)
 
-
-class MarkTBC(Opcode):
-    index: int
-
+class MarkTBC(LocalOpcode):
     def __init__(self, index: int):
-        super().__init__("mark_tbc")
-        self.index = index
+        super().__init__("mark_tbc", index)
 
     @property
     def arg_str(self) -> str:
         return f"{self.index}"
-
-    def comment_str(self, program: Program, proto: Prototype, pc) -> str:
-        return _format_local(self.index, proto)
