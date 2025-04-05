ae14d203ea69aedfa24c6ba066733df6cf9a49b4
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-04-05 12:56:05 +0300

    implement if statements

--- luark/compiler/ast/if_statement.py
+++ luark/compiler/ast/if_statement.py
@@ -1,16 +1,17 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 
 from lark.ast_utils import AsList
 from lark.tree import Meta
 
-from luark.compiler.ast import Block
+from luark.compiler.ast import AstNode, Block
 from luark.compiler.ast.expressions import Expression
 from luark.compiler.ast.statement import Statement
 from luark.compiler.compiler_state import CompilerState
+from luark.opcode.test import Test
 
 
 @dataclass
-class ElseIf:
+class ElseIf(AstNode):
     condition: Expression
     body: Block
 
@@ -19,18 +20,66 @@ class ElseIf:
 class IfStatement(Statement, AsList):
     condition: Expression
     body: Block
-    else_if_branches: list[ElseIf] = field(default_factory=list)
-    else_branch: Block | None = None
+    else_if_branches: list[ElseIf]
+    else_branch: Block | None
 
     def __init__(self, meta: Meta, children: list):
         self.meta = meta
         self.condition = children[0]
         self.body = children[1]
+
+        children = children[2:]
+        if not children:
+            self.else_if_branches = []
+            self.else_branch = None
+            return
+
         if isinstance(children[-1], Block):
-            self.else_if_branches = children[2:-1]
+            self.else_if_branches = children[:-1]
             self.else_branch = children[-1]
         else:
-            self.else_if_branches = children[2:]
+            self.else_if_branches = children
+            self.else_branch = None
 
     def compile(self, state: CompilerState) -> None:
-        raise NotImplementedError
+        skip_end_jump = (not self.else_branch) and len(self.else_if_branches) == 0
+        end_jumps = []
+        self._emit_branch(state, self.condition, self.body, skip_end_jump, end_jumps)
+        for i, branch in enumerate(self.else_if_branches):
+            self._emit_branch(
+                state,
+                branch.condition,
+                branch.body,
+                skip_end_jump,
+                end_jumps
+            )
+        if self.else_branch:
+            state.begin_block()
+            for stmt in self.else_branch.statements:
+                stmt.compile(state)
+            state.end_block()
+
+        for jump_pc in end_jumps:
+            state.set_jump(jump_pc)
+
+    def _emit_branch(
+            self,
+            state: CompilerState,
+            condition: Expression,
+            block: Block,
+            skip_end_jump: bool,
+            end_jumps: list[int]
+    ):
+        condition.evaluate(state)
+        state.add_opcode(Test.INSTANCE)
+        jump_pc = state.reserve_opcode()
+
+        state.begin_block()
+        for stmt in block.statements:
+            stmt.compile(state)
+        state.end_block()
+
+        if not skip_end_jump:
+            end_jumps.append(state.program_counter)
+            state.reserve_opcode()
+        state.set_jump(jump_pc)
--- tests/script.lua
+++ tests/script.lua
@@ -1,10 +1,4 @@
-function foo()
-    local x = 0
-    function bar()
-        function baz()
-            x = x + 1
-        end
-        return baz
-    end
-    return bar
-end
+-- if foo() then a() end
+-- if foo() then a() else b() end
+-- if foo() then a() elseif bar() then b() end
+if foo() then a() elseif bar() then b() else c() end
