42be2d21b697ed03e5a821399b110fc81faf7511
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-04-06 18:15:07 +0300

    reformat chunk.py

--- luark/compiler/ast/chunk.py
+++ luark/compiler/ast/chunk.py
@@ -2,11 +2,9 @@ from lark.tree import Meta
 
 from luark.compiler.ast.ast_node import AstNode
 from luark.compiler.ast.block import Block
-from luark.compiler.ast.function_definitions import (
-    FunctionBody,
-    FunctionDefinition, ParameterList
-)
+from luark.compiler.ast.function_definitions import FunctionBody, FunctionDefinition, ParameterList
 from luark.compiler.compiler_state import CompilerState
+from luark.program import Program
 
 
 class Chunk(AstNode):
@@ -17,7 +15,7 @@ class Chunk(AstNode):
         func_body = FunctionBody(meta, param_list, self.block)
         self.func_def = FunctionDefinition(meta, func_body, "<$main>")
 
-    def compile(self, state: CompilerState):
+    def compile(self, state: CompilerState) -> Program:
         # We compile the chunk as a variadic function with the same body.
         self.func_def.evaluate(state, create_closure=False)
         return state.compile()
