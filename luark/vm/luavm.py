4e9d631ec3677a51fe59aca5b609daa76fd205a9
imbeer <alekceykocenko@gmail.com>
2025-06-11 15:06:24 +0300

    add program runner and prototype runner classes

--- luark/vm/luavm.py
+++ luark/vm/luavm.py
@@ -1,3 +1,33 @@
+from dataclasses import dataclass
+
+from luark.opcode import Opcode
+from luark.program import Program, Prototype
+from luark.vm.types import AnyType
+
+
+@dataclass
+class PrototypeRunner:
+    prototype: Prototype
+    program_counter: int
+
+
+class ProgramRunner:
+    def __init__(self, program: Program):
+        self.value_stack: list[AnyType] = []
+        self.env: dict[int, AnyType]
+        self.call_stack: list[PrototypeRunner] = []
+        self.program: Program = program
+        self.push_prototype(program.prototypes[0])
+
+    def push_prototype(self, prototype: Prototype):
+        self.call_stack.append(
+            PrototypeRunner(
+                prototype=prototype,
+                program_counter=0
+            )
+        )
+
+
 class LuaVM:
     def __init__(self):
         self.call_stack = []
