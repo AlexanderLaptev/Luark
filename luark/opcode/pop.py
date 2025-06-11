567dd8f2a0a3ac87c7b533da3cfb5a4f64ba23f7
imbeer <alekceykocenko@gmail.com>
2025-06-11 18:57:22 +0300

    implement pop opcode

--- luark/opcode/pop.py
+++ luark/opcode/pop.py
@@ -1,6 +1,7 @@
 from typing import Self
 
 from luark.opcode import Opcode
+from luark.vm.luavm import ProgramRunner, PrototypeRunner
 
 
 class Pop(Opcode):
@@ -9,6 +10,10 @@ class Pop(Opcode):
     def __init__(self):
         assert Pop.INSTANCE is None
         super().__init__("pop")
+
+    def run(self, program_runner: ProgramRunner, prototype_runner: PrototypeRunner):
+        program_runner.value_stack.pop()
+        prototype_runner.step()
 
 
 Pop.INSTANCE = Pop()
