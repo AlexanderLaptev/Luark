7013d9d10b3125595ba3506b74134ce4040b5402
Alexander Laptev <alexander_laptev4@inbox.ru>
2025-06-12 19:29:09 +0300

    fix assignment statements

--- tests/script.lua
+++ tests/script.lua
@@ -1,7 +1,17 @@
--- function foo() return end
--- function foo() return 1 end
--- function foo() return 1, 2, 3 end
--- function foo() return 1, 2, ... end
--- function foo() return 1, 2, (...) end
--- function foo() return 1, 2, ...,  3 end
-function foo() return 1, 2, bar() end
+-- a = 1
+-- a = 1, 2
+-- a = ...
+-- a = foo()
+
+-- a, b = 1
+-- a, b = 1, 2
+-- a, b = 1, 2, 3, 4
+-- a, b = ...
+-- a, b = foo()
+-- a, b = (...)
+-- a, b = ..., ...
+-- a, b = 1, (...)
+-- a, b = 1, (foo())
+-- a, b = b, a
+
+i, a[i] = i+1, 20
