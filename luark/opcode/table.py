c67043b5b6f5a0e44d57e2e42511318a6c6b227e
imbeer <alekceykocenko@gmail.com>
2025-06-13 17:23:09 +0300

    implement table opcodes

--- luark/opcode/table.py
+++ luark/opcode/table.py
@@ -2,6 +2,9 @@ from typing import Self
 
 from luark.opcode import Opcode
 from luark.program import Program, Prototype
+from luark.vm.exception import TypeException
+from luark.vm.luavm import ProgramRunner, PrototypeRunner
+from luark.vm.types import Table
 
 
 class CreateTable(Opcode):
@@ -10,6 +13,11 @@ class CreateTable(Opcode):
     def __init__(self):
         assert CreateTable.INSTANCE is None
         super().__init__("create_table")
+
+    def run(self, program_runner: ProgramRunner, prototype_runner: PrototypeRunner):
+        value = Table()
+        program_runner.value_stack.append(value)
+        prototype_runner.step()
 
 
 CreateTable.INSTANCE = CreateTable()
@@ -22,6 +30,18 @@ class GetTable(Opcode):
         assert GetTable.INSTANCE is None
         super().__init__("get_table")
 
+    def run(self, program_runner: ProgramRunner, prototype_runner: PrototypeRunner):
+        table = program_runner.value_stack.pop()
+        key = program_runner.value_stack.pop()
+
+        if table is not Table:
+            raise TypeException(prototype_runner, message="Not a table")
+        assert isinstance(table, Table)
+
+        value = table.get(key)
+        program_runner.value_stack.append(value)
+        prototype_runner.step()
+
 
 GetTable.INSTANCE = GetTable()
 
@@ -32,6 +52,18 @@ class SetTable(Opcode):
     def __init__(self):
         assert SetTable.INSTANCE is None
         super().__init__("set_table")
+
+    def run(self, program_runner: ProgramRunner, prototype_runner: PrototypeRunner):
+        value = program_runner.value_stack.pop()
+        table = program_runner.value_stack.pop()
+        key = program_runner.value_stack.pop()
+
+        if table is not Table:
+            raise TypeException(prototype_runner, message="Not a table")
+        assert isinstance(table, Table)
+
+        table.set(key, value)
+        prototype_runner.step()
 
 
 SetTable.INSTANCE = SetTable()
