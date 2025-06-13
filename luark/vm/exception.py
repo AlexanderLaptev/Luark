2a2f3d3bc8804d505e23cd366e28c3d6f293e0eb
imbeer <alekceykocenko@gmail.com>
2025-06-13 10:15:30 +0300

    improve exception content

--- luark/vm/exception.py
+++ luark/vm/exception.py
@@ -1,10 +1,33 @@
-class NilPointerException(RuntimeError):
+from luark.vm.luavm import PrototypeRunner
+
+
+class BaseRuntimeException(RuntimeError):
+    def __init__(self, message: str = "", prototype_name: str = "", program_counter: int = 0):
+        self.message: str = message
+        self.prototype_name: str = prototype_name
+        self.program_counter: int = program_counter
+
+
+class NilPointerException(BaseRuntimeException):
     """A runtime error on access data by nil"""
-    pass
+
+    def __init__(self, prototype_runner: PrototypeRunner = None, message: str = "Cannot access to data by nil"):
+        super().__init__(
+            message,
+            prototype_runner.prototype.function_name if prototype_runner is not None else "",
+            prototype_runner.program_counter if prototype_runner is not None else 0)
 
 
-# class TypeException(RuntimeError):
-#     """ A runtime error that when types dont match"""
+class TypeException(BaseRuntimeException):
+    """ A runtime error that when types dont match"""
 
-class UnsupportedOperation(RuntimeError):
+    def __init__(self, prototype_runner: PrototypeRunner, message: str = "Types doesnt match"):
+        super().__init__(message, prototype_runner.prototype.function_name, prototype_runner.program_counter)
+
+
+class UnsupportedOperation(BaseRuntimeException):
     """ A runtime error when vm gets unsupported operation from compiler"""
+
+    def __init__(self, prototype_runner: PrototypeRunner,
+                 message: str = "This operation is not supported by virtual machine yet"):
+        super().__init__(message, prototype_runner.prototype.function_name, prototype_runner.program_counter)
